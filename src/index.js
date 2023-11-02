84b60b17d81ef38ef77176fa57f22fbfd181f008
tylersmith <tyjsmith1@gmail.com>
2023-11-02 15:44:31 -0500

    lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const callForm = document.getElementById('create-task-form')
+  const newList = document.getElementById('tasks')
+
+  callForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const li = document.createElement("li")
+    const input = document.getElementById("new-task-description").value
+    li.textContent = input
+    newList.appendChild(li)
+  })
 });
